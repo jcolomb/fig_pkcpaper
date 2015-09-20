63e3a542e3b0f3e7fb6620f1b053e994546ea26d
colombj <julien.colomb@fu-berlin.de>
2015-09-20 21:57:14 +0200

    initial
    code pushed to git

---
+++ rcode/graph/doallanalysis.R
@@ -0,0 +1,10 @@
+###do all analysis again
+subneg=TRUE
+setwd("/Users/choupi/Desktop/boulot/FSdata")
+source("graph/RNAi53_V_T.r")
+source("graph/RNAi53_inac.r")
+source("graph/PKCi_nophenotype.r")
+source("graph/PKCi_phenotype.r")
+source("graph/d42cha.r")
+source("graph/mildHS.r")
+source("graph/mutant_analysis.r")
